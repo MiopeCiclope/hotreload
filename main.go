cae9b27c102b198b448a9dd671fc7f46ce7b8b5b
Romulo Tone <romulo.tone@etraveligroup.com>
2023-06-21 10:54:13 +0200

    clueless

--- main.go
+++ main.go
@@ -5,12 +5,10 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
-	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
@@ -23,51 +21,6 @@ func check(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-// Build command for products package
-func runBuild(ctx context.Context) {
-	app := "run build -- products"
-	command := strings.Split(app, " ")
-	cmd := exec.CommandContext(ctx, "npm", command...)
-	toExecute := filepath.Dir(Path)
-	cmd.Dir = toExecute
-
-	outR, outW := io.Pipe()
-	cmd.Stdout = io.MultiWriter(outW, os.Stdout)
-	cmd.Stderr = os.Stderr
-
-	lines := make(chan string)
-	go func() {
-		for line := range lines {
-			fmt.Println(line)
-		}
-	}()
-
-	go func() {
-		defer close(lines)
-		scanner := bufio.NewScanner(outR)
-		for scanner.Scan() {
-			lines <- scanner.Text()
-		}
-		if err := scanner.Err(); err != nil {
-			fmt.Println(err)
-		}
-	}()
-
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	err = cmd.Wait()
-	_ = outW.Close()
-	if err != nil {
-		return
-	}
-
-	fmt.Println("Kept running")
 }
 
 // Find all folders inside root to watch
@@ -109,18 +62,19 @@ func main() {
 	// Create Watchers
 	toWatch := Path
 	watchRecursive(toWatch, watcher)
+	threadCounter := counter()
 	fmt.Println("watching dir: ", toWatch)
-
-	var daemon Daemon
-
+	quit := make(chan bool)
 	// Notify when a change happens
 	for {
 		select {
 		case event := <-watcher.Events:
 			if event.Op != fsnotify.Chmod {
 				// fmt.Println("Build Trigger:", event.String())
+				quit <- true
 				// Run build command
-				go daemon.Start()
+				daemon := getInstance()
+				go daemon.Start(threadCounter, quit)
 			}
 		case err, ok := <-watcher.Errors:
 			fmt.Println(ok, err)
@@ -128,77 +82,98 @@ func main() {
 	}
 }
 
+func counter() func() int {
+	sum := 0
+	return func() int {
+		sum++
+		return sum
+	}
+}
+
+// var lock = &sync.Mutex{}
+
+var singleInstance *Daemon
+
+func getInstance() *Daemon {
+	// lock.Lock()
+	if singleInstance == nil {
+		fmt.Println("Creating single instance now.")
+		singleInstance = &Daemon{}
+	} else {
+		singleInstance.Cancel()
+		fmt.Println("Single instance already created.")
+	}
+	// lock.Unlock()
+
+	return singleInstance
+}
+
 type Daemon struct {
 	cmdErr error
 	cancel func()
 }
 
-func (d *Daemon) Start() {
-	fmt.Println("Start")
-	if d.cancel != nil {
-		d.cancel()
-	}
+func (d *Daemon) Start(getId func() int, quit chan bool) {
+	tId := getId()
+	fmt.Println("Thread-", tId, " -> start")
 
-	ctx, cancel := context.WithCancel(context.Background())
-	d.cancel = cancel
+	select {
+	case <-quit:
+		fmt.Println("Thread-", tId, " Teje morto")
 
-	app := "run build -- products"
-	command := strings.Split(app, " ")
-	cmd := exec.CommandContext(ctx, "npm", command...)
-	toExecute := filepath.Dir(Path)
-	cmd.Dir = toExecute
+		return
+	default:
 
-	// outR, outW := io.Pipe()
-	// cmd.Stdout = io.MultiWriter(outW, os.Stdout)
-	// cmd.Stderr = os.Stderr
-	// lines := make(chan string)
-	// go func() {
-	// 	for line := range lines {
-	// 		fmt.Println(line)
-	// 	}
-	// }()
+		ctx, cancel := context.WithCancel(context.Background())
+		d.cancel = cancel
 
-	// go func() {
-	// 	defer close(lines)
-	// 	scanner := bufio.NewScanner(outR)
-	// 	for scanner.Scan() {
-	// 		lines <- scanner.Text()
-	// 	}
-	// 	if err := scanner.Err(); err != nil {
-	// 		fmt.Println("scanner: ", err)
-	// 	}
-	// }()
-	time.Sleep(500 * time.Millisecond)
-	err := cmd.Start()
-	if err != nil {
-		if cmd.Process != nil {
-			fmt.Println("Killing time")
-			cmd.Process.Kill()
+		app := "run build -- products"
+		command := strings.Split(app, " ")
+		cmd := exec.CommandContext(ctx, "npm", command...)
+
+		toExecute := filepath.Dir(Path)
+		cmd.Dir = toExecute
+
+		outR, outW := io.Pipe()
+		cmd.Stdout = io.MultiWriter(outW, os.Stdout)
+		cmd.Stderr = os.Stderr
+		lines := make(chan string)
+
+		go func(threadId int) {
+			for line := range lines {
+				fmt.Println("Thread-", threadId, " -> ", line)
+			}
+		}(tId)
+
+		go func(threadId int) {
+			scanner := bufio.NewScanner(outR)
+			for scanner.Scan() {
+				lines <- scanner.Text()
+			}
+			if err := scanner.Err(); err != nil {
+				fmt.Println("Thread-", threadId, " -> ", "scanner: ", err)
+			}
+			close(lines)
+		}(tId)
+
+		err := cmd.Start()
+		if err != nil {
+			fmt.Println("Thread-", tId, " -> ", "Fatal", err)
 		}
-		fmt.Println("Fatal", err)
-	}
 
-	go func() {
-		err := cmd.Wait()
-		// if cmd.Process != nil {
-		// 	cmd.Process.Kill()
-		// }
-		fmt.Println("command exited; error is:", err)
-		// _ = outW.Close() // TODO: handle error from Close(); log it maybe.
-		d.cmdErr = err
-	}()
+		go func(threadId int) {
+			err := cmd.Wait()
+			fmt.Println("Thread-", threadId, " -> ", "command exited; error is:", err)
+			_ = outW.Close() // TODO: handle error from Close(); log it maybe.
+			d.cmdErr = err
+		}(tId)
+	}
 }
 
-// Cancel causes the running command to exit preemptively.
-// If Cancel is called after the command has already
-// exited either naturally or due to a previous Cancel call,
-// then Cancel has no effect.
 func (d *Daemon) Cancel() {
 	d.cancel()
 }
 
-// CmdErr returns the error, if any, from the command's exit.
-// Only valid after the channel returned by Done() has been closed.
 func (d *Daemon) CmdErr() error {
 	return d.cmdErr
 }
